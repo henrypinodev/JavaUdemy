84be08e379c3d9b74af5a77d850de5297724dc21
Henry <henrypino93@gmail.com>
2025-06-22 22:05:11 -0400

    sin comentarios

--- src/main/java/ClaseMath/EjemploClaseMathRandom.java
+++ src/main/java/ClaseMath/EjemploClaseMathRandom.java
@@ -4,11 +4,20 @@ public class EjemploClaseMathRandom {
     public static void main(String[] args) {
 
         // el metodo random sin especificar solo devuelve un numero random entre 0 y 1 eje 0.564854651
+
+        String[] colores = {"azul", "amarillo", "verde", "Celeste"};
         double random = Math.random();
         System.out.println(random);
 
-        random = random *7;
+        random = random * colores.length;
         System.out.println(random);
+
+        random = Math.floor(random);
+        System.out.println("random= "+random);
+
+        System.out.println(random);
+
+        System.out.println("Colores = "+ colores[(int)random]);
 
     }
 }
