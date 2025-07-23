9e55a0b9c1d2ce6b965a73429a5f9cba3726ec59
Henry <henrypino93@gmail.com>
2025-07-23 01:24:49 -0400

    fix de double

--- src/main/java/Arreglos/EjemploArregloNotasAlumnos.java
+++ src/main/java/Arreglos/EjemploArregloNotasAlumnos.java
@@ -6,6 +6,8 @@ public class EjemploArregloNotasAlumnos {
     public static void main(String[] args) {
 
         double[] claseMatematicas, claseHistoria, claseLenguaje;
+        int sumNotasMatematicas = 0, sumNotasHistoria = 0, sumNotasLenguaje = 0;
+
 
         claseMatematicas = new double[7];
         claseLenguaje = new double[7];
@@ -30,8 +32,27 @@ public class EjemploArregloNotasAlumnos {
             claseHistoria[i] = scanner.nextDouble();
         }
 
+        for (int i = 0; i < 7; i++){
+            // si utilizo el += no debo castear el int debido a que es implicito para Java, asume
+            sumNotasMatematicas = (int) (sumNotasMatematicas + claseMatematicas[i]);
+            sumNotasHistoria = (int) (sumNotasHistoria + claseHistoria[i]);
+            sumNotasLenguaje = (int) (sumNotasLenguaje + claseLenguaje[i]);
+        }
 
+        double promedioMatematicas =  ((double) sumNotasMatematicas /claseMatematicas.length);
+        double promedioHistoria = ((double) sumNotasHistoria /claseHistoria.length);
+        double promedioLenguaje = ((double) sumNotasLenguaje /claseLenguaje.length);
+        double promedioGeneral = (sumNotasMatematicas+sumNotasHistoria+sumNotasLenguaje/3);
 
+        System.out.println("Promedio de notas matematicas: "+ promedioMatematicas);
+        System.out.println("Promedio de notas Historia: "+ promedioHistoria );
+        System.out.println("Promedio notas Lenguaje: "+ promedioLenguaje);
+        System.out.println("Promedio total de los tres cursos: "+ promedioGeneral);
+
+        System.out.println("Ingrese identificador del alumno( de 0 a 6)");
+        int id = scanner.nextInt();
+        double promedioAlumno = (claseMatematicas[id]+ claseHistoria[id]+ claseLenguaje[id]);
+        System.out.println("Promedio alumno es: "+ promedioAlumno+ " y su id es: "+ id);
 
     }
 }
